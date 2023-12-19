8219264fb69b6e94beb8bc5da8c6d1b98d3098e5
MengChunYou <angeladk394@gmail.com>
2023-12-20 05:27:38 +0800

    [Add] variable summary for 2 groups

--- src/descriptive_statistics.R
+++ src/descriptive_statistics.R
@@ -12,15 +12,102 @@ windowsFonts(JhengHei = windowsFont(family = "Microsoft JhengHei"))
 load("data/processed/study_area_polygons.RData")
 load("data/processed/processed_lease_records.RData")
 
-## Variable summary for 2 groups
+lease_sf <- lease_sf %>% st_drop_geometry() %>% 
+  mutate(`是否為店面` = factor(`是否為店面`, levels = c(T, F)))
 
-lease_sf %>% st_drop_geometry() %>% 
+## Variable summary for 2 groups
+variable_summary <- lease_sf %>% 
   group_by(`是否為店面`) %>% 
-  summarise(`屋齡` = mean(`屋齡`, na.rm = T),
-            `總樓層數` = mean(`總樓層數`, na.rm = T),
-            `租賃面積` = mean(`租賃面積`),
-            `到學校距離` = mean(`到學校距離`),
-            `到捷運站距離` = mean(`到捷運站距離`))
+  summarise(mean(`房間數`),
+            sd(`房間數`),
+            mean(`衛浴數`),
+            sd(`衛浴數`),
+            mean(`有無附傢俱` == "有"),
+            `有無附傢俱sd` = sqrt(
+              mean(`有無附傢俱` == "有")*(1 - mean(`有無附傢俱` == "有"))/(length(`有無附傢俱`) - 1)
+            ), 
+            mean(`有無管理組織` == "有"),
+            `有無管理組織sd` = sqrt(
+              mean(`有無管理組織` == "有")*(1 - mean(`有無管理組織` == "有"))/(length(`有無管理組織`) - 1)
+            ), 
+            mean(`到學校距離`),
+            sd(`到學校距離`),
+            mean(`到捷運站距離`),
+            sd(`到捷運站距離`),
+            mean(`到醫療機構距離`),
+            sd(`到醫療機構距離`),
+            mean(`屋齡`),
+            sd(`屋齡`),
+            mean(`總樓層數`),
+            sd(`總樓層數`),
+            mean(`租賃面積`),
+            sd(`租賃面積`),
+            mean(`是否為一樓`),
+            `是否為一樓sd` = sqrt(
+              mean(`是否為一樓`)*(1 - mean(`是否為一樓`))/(length(`是否為一樓`) - 1)
+            ),
+            length(`租賃年月日`)
+  ) %>% t() %>% as.data.frame() %>% .[-1, ]
+colnames(variable_summary) = c("Treatment", "Comparison")
+variable_summary <- variable_summary %>% 
+  mutate(Treatment = as.numeric(Treatment)) %>% 
+  mutate(Comparison = as.numeric(Comparison)) %>% 
+  mutate(Difference = ifelse(
+    grepl("mean", rownames(.)), 
+    round((`Treatment` - `Comparison`), 3),
+    ""
+  )
+  ) %>% 
+  mutate(Treatment = ifelse(
+    grepl("mean", rownames(.)) | grepl("length", rownames(.)), 
+    round(Treatment, 3),
+    paste("(", round(Treatment, 3), ")", sep = "")
+  )
+  ) %>% 
+  mutate(Comparison = ifelse(
+    grepl("mean", rownames(.)) | grepl("length", rownames(.)), 
+    round(Comparison, 3),
+    paste("(", round(Comparison, 3), ")", sep = "")
+  )
+  )
+
+variable_summary[2*c(1:11),3] <- c(
+  t.test(`房間數`~`是否為店面`, lease_sf)$stderr,
+  t.test(`衛浴數`~`是否為店面`, lease_sf)$stderr,
+  sqrt(
+    mean(lease_sf$`有無附傢俱`=="有")*(1-mean(lease_sf$`有無附傢俱`=="有"))*
+      (1/sum(lease_sf$`是否為店面`=="TRUE") + 1/sum(lease_sf$`是否為店面`=="FALSE"))
+  ),
+  sqrt(
+    mean(lease_sf$`有無管理組織`=="有")*(1-mean(lease_sf$`有無管理組織`=="有"))*
+      (1/sum(lease_sf$`是否為店面`=="TRUE") + 1/sum(lease_sf$`是否為店面`=="FALSE"))
+  ),
+  t.test(`到學校距離`~`是否為店面`, lease_sf)$stderr,
+  t.test(`到捷運站距離`~`是否為店面`, lease_sf)$stderr,
+  t.test(`到醫療機構距離`~`是否為店面`, lease_sf)$stderr,
+  t.test(`屋齡`~`是否為店面`, lease_sf)$stderr,
+  t.test(`總樓層數`~`是否為店面`, lease_sf)$stderr,
+  t.test(`租賃面積`~`是否為店面`, lease_sf)$stderr,
+  sqrt(
+    mean(lease_sf$`是否為一樓`)*(1-mean(lease_sf$`是否為一樓`))*
+      (1/sum(lease_sf$`是否為店面`=="TRUE") + 1/sum(lease_sf$`是否為店面`=="FALSE"))
+  )
+) %>% round(3) %>% paste("[", ., "]", sep = "")
+
+variable_summary %>% 
+  mutate(variable = c("房間數", "",
+                      "衛浴數", "",
+                      "有無附傢俱", "",
+                      "有無管理組織", "",
+                      "到學校距離", "",
+                      "到捷運站距離", "",
+                      "到醫療機構距離", "",
+                      "屋齡", "",
+                      "總樓層數", "",
+                      "租賃面積", "",
+                      "是否為一樓", "",
+                      "# of buildings"), .before = "Treatment") %>% 
+  write.csv("outputs/descriptive_statistics/variable_summary_for_2_groups.csv", row.names = F)
 
 ## Lease records map
 
